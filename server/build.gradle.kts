782e71aaa8af93b0637b9fded77a58ad1f1f8354
Omar Emara <mail@OmarEmara.dev>
2021-12-13 16:01:55 +0200

    Add basic Postgres JDBC support

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -6,11 +6,12 @@ application {
   mainModule.set("dev.omaremara.auctionsystem.server")
   mainClass.set("dev.omaremara.auctionsystem.server.Main")
   applicationDefaultJvmArgs = System.getProperties().
-    filter({(key, value) -> key.toString().startsWith("server")}).
+    filter({(key, value) -> key.toString().startsWith("auctionSystem")}).
     map({(key, value) -> "-D$key=$value"})
 }
 
 dependencies {
   implementation(project(":model"))
   implementation("com.google.code.gson:gson:2.8.9")
+  implementation("org.postgresql:postgresql:42.3.1")
 }
