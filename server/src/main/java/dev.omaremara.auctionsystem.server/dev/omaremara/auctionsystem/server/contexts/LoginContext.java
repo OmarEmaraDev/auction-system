d357c428cd5fa62d09c7b25857168718bbaabb7a
Mohamed Alkhateb <alkhateb118@gmail.com>
2021-12-13 21:50:10 +0200

    Add "LoginContext" class

--- server/src/main/java/dev.omaremara.auctionsystem.server/dev/omaremara/auctionsystem/server/contexts/LoginContext.java
+++ server/src/main/java/dev.omaremara.auctionsystem.server/dev/omaremara/auctionsystem/server/contexts/LoginContext.java
@@ -1,4 +1,4 @@
-package dev.omaremara.auctionsystem.server.contexts;
+package dev.omaremara.auctionsystem.server.dev.omaremara.auctionsystem.server.contexts;
 
 import java.io.*;
 import com.sun.net.httpserver.HttpExchange;
@@ -11,23 +11,18 @@ import dev.omaremara.auctionsystem.model.requests.LoginRequest;
 public class LoginContext implements HttpHandler {
     @Override
     public void handle(HttpExchange t) throws IOException {
-        // System.out.println(t.getRequestHeaders());
         int length = Integer.parseInt(t.getRequestHeaders().getFirst("Content-Length"));
         InputStream is = t.getRequestBody();
         byte[] bytes = is.readNBytes(length);
-        // String str = new String(bytes, StandardCharsets.UTF_8);
-        String str = "{\"name\":\"testName\", \"password\":\"testPassword\"}";
+        String str = new String(bytes, StandardCharsets.UTF_8);
         Gson gson = new Gson();
-        System.out.println("Problem1");
-        System.out.println(str);
         LoginRequest login = gson.fromJson(str, LoginRequest.class);
-        System.out.println("Problem2");
         System.out.println(login.name);
-        // System.out.println(login);
-        // String response = "This is the response";
-        // t.sendResponseHeaders(200, response.length());
-        // OutputStream os = t.getResponseBody();
-        // os.write(response.getBytes());
-            // os.close();
+        System.out.println(login.password);
+        String response = "This is the response";
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
