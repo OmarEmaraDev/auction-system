5e8902e41fe42b3fdbf802668ad793999b0c00dd
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-12-14 20:17:08 +0200

    Added Controller

--- client/src/main/java/dev.omaremara.auctionsystem.client/dev/omaremara/auctionsystem/client/controller/LoginController.java
+++ client/src/main/java/dev.omaremara.auctionsystem.client/dev/omaremara/auctionsystem/client/controller/LoginController.java
@@ -1,12 +1,37 @@
 package dev.omaremara.auctionsystem.client.controller;
 
+import java.io.IOException;
 import dev.omaremara.auctionsystem.client.util.ViewUtil;
 import dev.omaremara.auctionsystem.client.view.RegisterView;
 import javafx.scene.control.Label;
+import dev.omaremara.auctionsystem.client.Main;
+import dev.omaremara.auctionsystem.model.requests.LoginRequest;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpRequest.BodyPublishers;
 
 public class LoginController {
   public void login(String email, String password, Label errorLabel) {
     // TODO: Executed when the user logins.
+    LoginRequest login =new LoginRequest(email,password);
+    String s= Main.gson.toJson(login,LoginRequest.class);
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("https://localhost:8000/api/login/"))
+        .header("Content-Type", "application/json")
+        .POST(BodyPublishers.ofString(s))
+        .build();
+        
+        try{
+          HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        
+        }catch(Exception e){
+           throw new RuntimeException(e) ;       }
   }
 
   public void registerView() { ViewUtil.setSceneRoot(new RegisterView()); }
--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module dev.omaremara.auctionsystem.client {
   requires java.base;
   requires javafx.base;
+  requires java.net.http;
   requires javafx.controls;
   requires javafx.graphics;
   requires com.google.gson;
   requires dev.omaremara.auctionsystem.model;
   exports dev.omaremara.auctionsystem.client to javafx.graphics;
+
 }
