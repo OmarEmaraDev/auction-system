850f3aa34a571e2baaf5654282a0cf862ee897b4
Mohamed Alkhateb <alkhateb118@gmail.com>
2021-12-15 23:35:26 +0200

    Add LoginContext class in contexts folder, and LoginException class in exception.

--- server/src/main/java/dev.omaremara.auctionsystem.server/dev/omaremara/auctionsystem/server/contexts/LoginContext.java
+++ server/src/main/java/dev.omaremara.auctionsystem.server/dev/omaremara/auctionsystem/server/contexts/LoginContext.java
@@ -1,28 +1,72 @@
-package dev.omaremara.auctionsystem.server.dev.omaremara.auctionsystem.server.contexts;
+package dev.omaremara.auctionsystem.server.contexts;
 
-import java.io.*;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import java.nio.charset.StandardCharsets;
-import com.google.gson.Gson;
+import dev.omaremara.auctionsystem.model.User;
 import dev.omaremara.auctionsystem.model.requests.LoginRequest;
-// import org.json.simple.JSONObject;
+import dev.omaremara.auctionsystem.server.Main;
+import dev.omaremara.auctionsystem.server.exceptions.LoginException;
+import dev.omaremara.auctionsystem.model.responses.ErrorResponse;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+
 
 public class LoginContext implements HttpHandler {
+    private User getUser(LoginRequest loginRequest) throws LoginException {
+        String connectionURL = System.getProperty("auctionSystem.JDBC.connection.url");
+        try (Connection connection = DriverManager.getConnection(connectionURL)) {
+            String query = "SELECT * FROM users WHERE name = ?";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(1, loginRequest.name);
+                try (ResultSet result = statement.executeQuery()) {
+                    if (!result.isBeforeFirst()) {
+                        throw new LoginException("No user exists with this name.");
+                    } else {
+                        result.next();
+                        String password = result.getString("password");
+                        if(password.equals(loginRequest.password)){
+                            return new User(Integer.parseInt(result.getString("id")), result.getString("name"), result.getString("password"));
+                        }
+                        else {
+                            throw new LoginException("Wrong password");
+                        }
+                    }
+                } catch (SQLException exception) {
+                    throw new RuntimeException("Couldn't retrieve user from database!", exception);
+                }
+            } catch (SQLException exception) {
+                throw new RuntimeException("Couldn't query from database!", exception);
+            }
+        } catch (SQLException exception) {
+            throw new RuntimeException("Couldn't establish connection to database!", exception);
+        }
+    }
+
     @Override
-    public void handle(HttpExchange t) throws IOException {
-        int length = Integer.parseInt(t.getRequestHeaders().getFirst("Content-Length"));
-        InputStream is = t.getRequestBody();
-        byte[] bytes = is.readNBytes(length);
-        String str = new String(bytes, StandardCharsets.UTF_8);
-        Gson gson = new Gson();
-        LoginRequest login = gson.fromJson(str, LoginRequest.class);
-        System.out.println(login.name);
-        System.out.println(login.password);
-        String response = "This is the response";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+    public void handle(HttpExchange exchange) throws IOException {
+        InputStreamReader requestReader = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
+        LoginRequest loginRequest = Main.gson.fromJson(requestReader, LoginRequest.class);
+
+        String response;
+        try {
+           User user;
+           user = getUser(loginRequest);
+            response = Main.gson.toJson(user, User.class);
+            exchange.sendResponseHeaders(200, response.length());
+
+        } catch (LoginException exception){
+            ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+            response = Main.gson.toJson(errorResponse, ErrorResponse.class);
+            exchange.sendResponseHeaders(401, response.length());
+        }
+        OutputStreamWriter writer = new OutputStreamWriter(exchange.getResponseBody(), StandardCharsets.UTF_8);
+        writer.write(response);
+
+        writer.flush();
+        exchange.close();
     }
 }
