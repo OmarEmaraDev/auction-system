8411bd3bda657d61242bbf27c6415d8b28aade80
Abdelrahman Moharram <41553398+Abdelrahman-Moharram@users.noreply.github.com>
2021-12-23 19:27:18 +0200

    Implemenet lot list view

--- client/src/main/java/dev.omaremara.auctionsystem.client/dev/omaremara/auctionsystem/client/controller/LotListController.java
+++ client/src/main/java/dev.omaremara.auctionsystem.client/dev/omaremara/auctionsystem/client/controller/LotListController.java
@@ -2,6 +2,7 @@ package dev.omaremara.auctionsystem.client.controller;
 
 import dev.omaremara.auctionsystem.client.util.ViewUtil;
 import dev.omaremara.auctionsystem.client.view.BidView;
+import dev.omaremara.auctionsystem.client.view.NewLotView;
 import dev.omaremara.auctionsystem.model.Lot;
 import dev.omaremara.auctionsystem.model.User;
 import java.util.ArrayList;
@@ -19,5 +20,7 @@ public class LotListController {
                    new Lot(2, "Ball", new User(3, "Mark", ""), null, 30));
   }
 
-  public void bid(Lot lot) { ViewUtil.setSceneRoot(new BidView(lot)); }
+  public static void newLot() { ViewUtil.setSceneRoot(new NewLotView()); }
+
+  public static void bid(Lot lot) { ViewUtil.setSceneRoot(new BidView(lot)); }
 }
--- client/src/main/java/dev.omaremara.auctionsystem.client/dev/omaremara/auctionsystem/client/view/LotListView.java
+++ client/src/main/java/dev.omaremara.auctionsystem.client/dev/omaremara/auctionsystem/client/view/LotListView.java
@@ -2,13 +2,55 @@ package dev.omaremara.auctionsystem.client.view;
 
 import dev.omaremara.auctionsystem.client.controller.LotListController;
 import dev.omaremara.auctionsystem.client.view.View;
+import dev.omaremara.auctionsystem.model.Lot;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
-import javafx.scene.layout.Region;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class LotListView implements View {
   public Parent getRoot() {
-    // TODO: Displays all lots with their full information.
-    // A button to create a new lot and a button to make a bid on a certain lot.
-    return new Region();
+    Label errorLabel = new Label();
+    errorLabel.setTextFill(Color.RED);
+
+    Label headerLabel = new Label("Current Lots:");
+    headerLabel.setStyle("-fx-font-size : 40px;");
+
+    ObservableList<Lot> items = FXCollections.observableArrayList(
+        LotListController.getAllLots(errorLabel));
+    ListView<Lot> list = new ListView<Lot>();
+    list.setItems(items);
+    list.setCellFactory(targetList -> new ListCell<Lot>() {
+      public void updateItem(Lot lot, boolean empty) {
+        super.updateItem(lot, empty);
+        if (lot == null || empty) {
+          return;
+        }
+
+        BorderPane layout = new BorderPane();
+        Label titleLabel = new Label(lot.title);
+        titleLabel.setStyle("-fx-font-size : 20px;");
+        Button bidButton = new Button("Bid");
+        bidButton.setOnAction(e -> LotListController.bid(lot));
+        layout.setLeft(titleLabel);
+        layout.setRight(bidButton);
+        setGraphic(layout);
+      }
+    });
+
+    Button newLotButton = new Button("New Lot");
+    newLotButton.setOnAction(e -> LotListController.newLot());
+
+    VBox layout = new VBox(10);
+    layout.setPadding(new Insets(20, 80, 0, 80));
+    layout.getChildren().addAll(headerLabel, list, errorLabel, newLotButton);
+    return layout;
   }
 }
